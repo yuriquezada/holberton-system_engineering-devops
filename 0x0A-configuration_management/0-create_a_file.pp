742279e4eed1bf7925a1d95a2eeb057546fb27ef
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2022-01-03 23:35:57 -0500

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
-# Create a file in /tmp
+# Create a file
 
-file { 'holberton':
-    ensure  => 'present',
-    content => 'I love Puppet',
-    group   => 'www-data',
-    mode    => '0744',
-    owner   => 'www-data',
-    path    => '/tmp/holberton',
+file {'Create a file':
+    path    => '/tmp/school',
+    mode    =>  '0744',
+    owner   =>  www-data,
+    group   =>  www-data,
+    content =>  'I love Puppet',
 }
