888d6141a5aae21133b453a71f5de2123bc01b10
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2022-01-31 22:56:25 -0500

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,23 +1,38 @@
 #!/usr/bin/python3
-"""Exports data in the CSV format"""
-
+'''
+Consume API with Python to export to csv
+'''
+import csv
+from email.quoprimime import quote
+import requests
+from sys import argv
 if __name__ == "__main__":
+    employeeID = argv[1]
 
-    import csv
-    import requests
-    import sys
+    url = 'https://jsonplaceholder.typicode.com/users/{}/'.format(employeeID)
+    employee = requests.get(url)
 
-    userId = sys.argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                        .format(userId))
-    name = user.json().get('username')
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    route = 'https://jsonplaceholder.typicode.com/users/{}/todos'
+    url = route.format(employeeID)
+    todoByEmployee = requests.get(url)
 
-    filename = userId + '.csv'
-    with open(filename, mode='w') as f:
-        writer = csv.writer(f, delimiter=',', quotechar='"',
-                            quoting=csv.QUOTE_ALL, lineterminator='\n')
-        for task in todos.json():
-            if task.get('userId') == int(userId):
-                writer.writerow([userId, name, str(task.get('completed')),
-                                 task.get('title')])
+    username = employee.json()['username']
+    rows = []
+    data = {}
+
+    for i in todoByEmployee.json():
+        data = {}
+        data['USER_ID'] = employeeID
+        data['USERNAME'] = username
+        data['TASK_COMPLETED_STATUS'] = i['completed']
+        data['TASK_TITLE'] = i['title']
+        rows.append(data)
+
+    file = '{}.csv'.format(employeeID)
+
+    with open(file, 'w') as f:
+        fieldnames = [
+            'USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
+        writer = csv.DictWriter(
+            f, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
+        writer.writerows(rows)
