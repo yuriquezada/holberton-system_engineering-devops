9f8e9f6e0bf57c44451393c116e486cab5545102
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2022-01-31 22:49:12 -0500

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python3
 '''
-Return to-do list information for a given employee ID.
+Consume API with Python
 '''
 import requests
 from sys import argv
-
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    employeeID = argv[1]
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    url = 'https://jsonplaceholder.typicode.com/users/{}/'.format(employeeID)
+    employee = requests.get(url)
+
+    route = 'https://jsonplaceholder.typicode.com/users/{}/todos'
+    url = route.format(employeeID)
+    todoByEmployee = requests.get(url)
+
+    name = employee.json()['name']
+    completed = 0
+    tasks = 0
+
+    for i in todoByEmployee.json():
+        tasks += 1
+        if i['completed']:
+            completed += 1
+    text = 'Employee {} is done with tasks({:d}/{:d}):'
+    print(text.format(name, completed, tasks))
+    for i in todoByEmployee.json():
+        if i['completed']:
+            print('\t {}'.format(i['title']))
